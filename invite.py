7aa35b0adf3c2407d43d736fd1926a9cb9cc1144
Jacob Kuenzel <jayekub@gmail.com>
2024-01-10 22:54:30 -0800

    towards emailed invites, a bit of details work too

---
+++ invite.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import smtplib
+
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+sender = 'jayekub@gmail.com'
+recipients = ['jacobk@pixar.com']
+
+message = MIMEMultipart('alternative')
+message['From'] = sender
+message['To'] = ','.join(recipients)
+message['Subject'] = "You're Invited to Jacob's Roaring 40th!"
+
+message.attach(MIMEText(open('invite.txt', 'r').read(), 'plain'))
+message.attach(MIMEText(open('invite.html', 'r').read(), 'html'))
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+    smtp_server.login(sender, 'eqpk injg zaeq ylpu')
+    smtp_server.sendmail(sender, recipients, message.as_string())
