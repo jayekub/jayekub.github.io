34751a874407d885442858f1288831cdf708fd6c
Jacob Kuenzel <jayekub@gmail.com>
2024-01-10 16:14:27 -0800

    Updates

--- dot.sh
+++ dot.sh
@@ -1,11 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 # based on https://www.atlassian.com/git/tutorials/dotfiles
+
+cd $HOME
 
 git clone --bare git@github.com:jayekub/dot.git $HOME/.dot
 
-alias dot='git --git-dir=$HOME/.dot/ --work-tree=$HOME'
+function dot {
+    git --git-dir=$HOME/.dot/ --work-tree=$HOME $@
+}
 
-cd $HOME
 dot config --local status.showUntrackedFiles no
 dot checkout
 
@@ -14,8 +17,8 @@ if [ $? = 0 ]; then
 else
     echo "Backing up existing dotfiles."
 
-    mkdir -p .dot-backup
-    dot checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dot-backup/{}
+    mkdir -p $HOME/.dot-backup
+    dot checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $HOME/.dot-backup/{}
     dot checkout
 fi
 
